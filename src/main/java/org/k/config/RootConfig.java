38a6580aaf9ba538eab78d9ae7228f92fe1b7ae8
k <k@qq.com>
2023-03-11 23:39:08 +0800

    Changes

---
+++ src/main/java/org/k/config/RootConfig.java
@@ -0,0 +1,9 @@
+package org.k.config;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ComponentScan("org.k")
+public class RootConfig {
+}
---
+++ src/main/java/org/k/config/WebConfig.java
@@ -0,0 +1,30 @@
+package org.k.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.View;
+import org.springframework.web.servlet.ViewResolver;
+import org.springframework.web.servlet.config.annotation.DefaultServletHandlerConfigurer;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.view.InternalResourceViewResolver;
+
+@Configuration
+@EnableWebMvc
+@ComponentScan("org.k.controller")
+public class WebConfig implements WebMvcConfigurer {
+    @Bean
+    public ViewResolver viewResolver(){
+        InternalResourceViewResolver internalResourceViewResolver=
+                new InternalResourceViewResolver();
+        internalResourceViewResolver.setPrefix("/WEB-INF/");
+        internalResourceViewResolver.setSuffix(".jsp");
+        internalResourceViewResolver.setExposeContextBeansAsAttributes(true);
+        return internalResourceViewResolver;
+    }
+
+    public void configureDefaultServletHandling(DefaultServletHandlerConfigurer configurer) {
+        configurer.enable();
+    }
+}
