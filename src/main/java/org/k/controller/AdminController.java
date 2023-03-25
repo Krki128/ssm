11965ff4ad0af2acae9f1f54a7b88a278141debb
k <k@qq.com>
2023-03-25 10:41:06 +0800

    Changes

--- src/main/java/org/k/controller/AdminController.java
+++ src/main/java/org/k/controller/AdminController.java
@@ -4,10 +4,7 @@ import org.k.dao.Admin;
 import org.k.service.AdminService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 
 @Controller
 //@RequestMapping()
