91c6a933f8b06fc06c6d3cbb14485424cccf8ba9
k <k@qq.com>
2023-04-06 17:07:44 +0800

    Changes
    Signed-off-by: k <k@qq.com>

--- src/main/java/org/k/config/GeneratorSqlMap.java
+++ src/main/java/org/k/config/GeneratorSqlMap.java
@@ -1,24 +1,16 @@
 package org.k.config;
 
-import com.alibaba.druid.pool.DruidDataSource;
-import com.alibaba.druid.sql.visitor.functions.Char;
 import org.mybatis.generator.api.MyBatisGenerator;
 import org.mybatis.generator.config.Configuration;
 import org.mybatis.generator.config.xml.ConfigurationParser;
 import org.mybatis.generator.internal.DefaultShellCallback;
-import org.springframework.beans.factory.annotation.Autowired;
-
-import javax.sql.DataSource;
 import java.io.File;
-import java.sql.SQLException;
 import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
 
 public class GeneratorSqlMap {
     static void generator() throws Exception{
-        List<String> warnings= new ArrayList<String>();
+        List<String> warnings= new ArrayList<>();
         boolean overwrite=true;
         File configFile=new File("C:\\Users\\26591\\Documents\\tool\\Code\\Java\\ssm\\src\\main\\resources\\GeneratorConfig.xml");
         ConfigurationParser  configurationParser=new ConfigurationParser(warnings);
